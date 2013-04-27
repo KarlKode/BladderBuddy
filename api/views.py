3877587825df7adca79d70e46019174fa34bd297
KarlKode <marc@marcg.ch>
2013-04-27 18:01:03 +0200

    bug

--- api/views.py
+++ api/views.py
@@ -32,31 +32,31 @@ def toilet():
         t = Toilet.query.get_or_404(id)
         return jsonify(toilet=t.__json__())
     if request.method == 'PUT':
+        title = request.form.get('title')
+        if not title:
+            raise ValueError('Invalid title')
+        description = request.form.get('description')
+        address = request.form.get('address')
+        category_id = int(request.form.get('category'))
+        lat = float(request.form.get('lat'))
+        lng = float(request.form.get('lng'))
         try:
-            title = request.form.get('title')
-            if not title:
-                raise ValueError('Invalid title')
-            description = request.form.get('description')
-            address = request.form.get('address')
-            category_id = int(request.form.get('category'))
-            lat = float(request.form.get('lat'))
-            lng = float(request.form.get('lng'))
-            price = int(request.form.get('price'))
-            code = request.form.get('code')
-            time_open = request.form.get('time_open')
-            time_close = request.form.get('time_close')
-            t = Toilet(title, lat, lng)
-            t.description = description
-            t.address = address
-            t.category_id = category_id
-            t.price = price
-            t.code = code
-            print request.form
-            print time_open
-            #t.time_open = time_open
-            #t.time_close = time_close
-            db.session.add(t)
-            db.session.commit()
-            return jsonify(success='success')
+            price = int(request.form.get('price', 0))
         except:
-            raise abort(500)
+            price = 0
+        code = request.form.get('code')
+        time_open = request.form.get('time_open')
+        time_close = request.form.get('time_close')
+        t = Toilet(title, lat, lng)
+        t.description = description
+        t.address = address
+        t.category_id = category_id
+        t.price = price
+        t.code = code
+        print request.form
+        print time_open
+        #t.time_open = time_open
+        #t.time_close = time_close
+        db.session.add(t)
+        db.session.commit()
+        return jsonify(success='success')
